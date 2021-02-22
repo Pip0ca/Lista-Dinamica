10b0157a585ef2d2cb1540a1e4a2602a6f124b33
Bruna de Paiva Gonçalves <65919877+Pip0ca@users.noreply.github.com>
2021-02-22 16:03:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,45 @@
-var ul = documento.getElementById("lista");
+var ul = document.getElementById("lista");
+//buscando elemento do html - lista
 var li;
 var itemId;
+var item;
 
 addAnim = function(){
-alert("oi");
+    if(document.getElementById("animText").value !=""){
+    //verificar se a caixa de texto não esta vazio
+        item = document.getElementById("animText");
+        itemId = ul.childElementCount; 
+        // childElementCount quantifade de elementos filhos
+        li = createItemEl(item.value,itemId);
+        li.appendChild(createRemoveAnimBtn(itemId));
+        ul.appendChild(li);
+    }
+    
 }
 
-removeAnim = function(){
-
+removeAnim = function(itemId){
+    for (i=0; i<ul.children.length ; i++){
+        if(ul.children[i].getAttribute("index") == itemId){
+            ul.children[i].remove();
+        }
+    }
 }
 
-createItemEl = function(){
-
+createItemEl = function(itemValue,itemId){
+    let li = document.createElement("li");
+    //criar elemento item de lista
+    li.setAttribute("index", itemId);
+    li.appendChild(document.createTextNode(itemValue));
+    return li;
 }
 
-createRemoveAnimBtn = function(){
+createRemoveAnimBtn = function(itemId){
+    let btn = document.createElement("button");
+    btn.setAttribute("onclick", "removeAnim("+itemId+")");
+    btn.innerHTML = "X";
+    return btn;
+}
 
+contatAnim = function(ul){
+    document.getElementById("contador").innerHTML = itemId + 1;
 }
